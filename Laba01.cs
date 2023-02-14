645bc89c5e300bad1766939210d812e996fc7cf3
We1R <veter445566@mail.ru>
2023-02-14 16:09:54 +0700

    changed the name of variables in Laba01.cs

--- Laba01.cs
+++ Laba01.cs
@@ -4,13 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/**********************************
- * Автор: Козлов Егор             * 
- * Тема: Базовые понятия языка С# *
- **********************************/
-
-namespace Laba01 {
-  internal class Laba01 {
+namespace Laba1 {
+  internal class Program {
     static void Main(string[] args) {
       Console.WriteLine("Введите номер задания :");
       int task = int.Parse(Console.ReadLine());
@@ -24,29 +19,27 @@ namespace Laba01 {
 
         double result = 1;
 
-        for (int i = 0; i < n; ++i) {
+        for (int numberCount = 0; numberCount < n; ++numberCount) {
           result *= a;
         }
 
         Console.WriteLine("a^n = " + result);
         Console.ReadKey();
-      }
-
-      if (task == 2) {
+      } else if (task == 2) {
         Console.WriteLine("Введите число состоящее из трёх или более цифр :");
         int number = int.Parse(Console.ReadLine());
 
         if (number >= 100) {
           int digitCount = (int)Math.Log10(number) + 1;
 
-          string s = number.ToString();
-          string result = s[s.Length - digitCount].ToString();
-          for (int i = digitCount; i >= 1; --i) {
-            if (i != (digitCount - 1) && i != digitCount) {
-              result += s[s.Length - i].ToString();
+          string str = number.ToString();
+          string result = str[str.Length - digitCount].ToString();
+          for (int digitPosition = digitCount; digitPosition >= 1; --digitPosition) {
+            if (digitPosition != (digitCount - 1) && digitPosition != digitCount) {
+              result += str[str.Length - digitPosition].ToString();
             }
           }
-          result += s[s.Length - (digitCount - 1)].ToString();
+          result += str[str.Length - (digitCount - 1)].ToString();
 
           Console.WriteLine(result);
           Console.ReadKey();
@@ -54,6 +47,9 @@ namespace Laba01 {
           Console.WriteLine("В этом числе менее трёх цифр");
           Console.ReadKey();
         }
+      } else {
+        Console.WriteLine("Такого задания нет");
+        Console.ReadKey();
       }
     }
   }
